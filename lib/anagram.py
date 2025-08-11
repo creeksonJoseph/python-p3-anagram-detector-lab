bb6a7e42259e3cff4e3859da4609e4b146977496
creeksonjoseph <charanajoseph@gmail.com>
2025-08-11 07:03:20 +0300

    h

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
-# your code goes here!
+
+
+class Anagram:
+    def __init__(self, word):
+        if not isinstance(word, str):
+            raise ValueError("Word must be a string")
+        self.word = sorted(word.lower()) 
+
+    def match(self, word_list):
+        if not isinstance(word_list, list):
+            raise ValueError("Input must be a list")
+
+        matches = []
+        for w in word_list:
+            if sorted(w.lower()) == self.word:
+                matches.append(w)
+        return matches
